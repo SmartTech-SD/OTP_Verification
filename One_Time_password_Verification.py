a69dbc3f580cf16632407fa5f59d26867088fbb2
Shubhankar Das <76389456+SmartTech-SD@users.noreply.github.com>
2020-12-20 16:09:06 +0530

    Update One_Time_password_Verification.py

--- One_Time_password_Verification.py
+++ One_Time_password_Verification.py
@@ -1,19 +1,17 @@
 import random
 from twilio.rest import Client
 otp=random.randint(100000,1000000)
-print(otp)
 # twilio account and token
-account_sid='AC1cd45e881aa4008a3ec7a1d2d8e1f582'
-auth_token='5e958a39090348c18a5c9100328dec67'
+account_sid=''
+auth_token=''
 client=Client(account_sid,auth_token)
 # user input
 mobile_no=input("Enter your mobile number with cuntry code: ")
-print(mobile_no)
 # for sending otp verification message
 message=client.messages.create(
 body=f"Your otp verification code is :{str(otp)} Don't share this code with others ",
 to=mobile_no,
-from_='+12517584907'
+from_='+125175849078090'
 )
 print(message.sid)
 while True:
